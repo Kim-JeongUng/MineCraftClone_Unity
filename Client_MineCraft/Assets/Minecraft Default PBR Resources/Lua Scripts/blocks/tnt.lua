657b64c11e016300b468991b25b0d5e6c95a31e8
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2026-03-30 12:17:01 +0900

    Fix synchronized TNT fuse blinking across clients

--- Client_MineCraft/Assets/Minecraft Default PBR Resources/Lua Scripts/blocks/tnt.lua
+++ Client_MineCraft/Assets/Minecraft Default PBR Resources/Lua Scripts/blocks/tnt.lua
@@ -4,12 +4,14 @@ local util = require "xlua.util"
 tnt = create_block_behaviour()
 local unityTime = CS.UnityEngine.Time
 local playerModification = CS.Minecraft.ModificationSource.PlayerAction
+local systemModification = CS.Minecraft.ModificationSource.InternalOrSystem
 local quaternionIdentity = CS.UnityEngine.Quaternion.identity
 local ignoreExplosionsFlag = CS.Minecraft.Configurations.BlockFlags.IgnoreExplosions
 local assetManager = CS.Minecraft.Assets.AssetManager.Instance
 local explosionEffectAssetName = "Assets/Minecraft Default PBR Resources/Effects/Explosion Effect.prefab"
 local waitTime = 3
 local explodeRadius = 5
+local blinkInterval = 0.2
 
 function tnt:init(world, block)
     tnt.base.init(self, world, block)
@@ -49,19 +51,43 @@ function tnt:click(x, y, z)
 
     self.pending_explosions[key] = true
     self.world:StartCoroutine(util.cs_generator(function()
-        local time = 0
+        local elapsed = 0
+        local blinkElapsed = 0
+        local showBlock = true
         local effectAsset = assetManager:LoadAsset(explosionEffectAssetName, typeof(CS.UnityEngine.GameObject))
         local accessor = self.world.RWAccessor
 
-        while time < waitTime or not effectAsset.IsDone do
-            time = time + unityTime.deltaTime
+        while elapsed < waitTime do
+            local current = accessor:GetBlock(x, y, z)
+            if not current or (current.InternalName ~= self.InternalName and current.InternalName ~= self.air_block_data.InternalName) then
+                self.pending_explosions[key] = nil
+                return
+            end
+
+            elapsed = elapsed + unityTime.deltaTime
+            blinkElapsed = blinkElapsed + unityTime.deltaTime
+
+            if blinkElapsed >= blinkInterval then
+                blinkElapsed = 0
+                showBlock = not showBlock
+                accessor:SetBlock(
+                    x,
+                    y,
+                    z,
+                    showBlock and self:get_block_data() or self.air_block_data,
+                    quaternionIdentity,
+                    systemModification)
+            end
+
             coroutine.yield(nil)
         end
 
         local block = accessor:GetBlock(x, y, z)
 
         -- 이미 다른 폭발로 제거된 경우 중복 처리하지 않음
-        if block and block.InternalName == self.InternalName then
+        if block and (block.InternalName == self.InternalName or block.InternalName == self.air_block_data.InternalName) then
+            accessor:SetBlock(x, y, z, self:get_block_data(), quaternionIdentity, systemModification)
+
             -- 물속 폭발은 블록을 파괴하지 않음
             if block.InternalName ~= self.water_name then
                 self:explode(x, y, z, explodeRadius, accessor)
@@ -70,11 +96,13 @@ function tnt:click(x, y, z)
             accessor:SetBlock(x, y, z, self.air_block_data, quaternionIdentity, playerModification)
         end
 
-        local effect = CS.UnityEngine.Object.Instantiate(effectAsset.Asset)
-        effect.transform.position = CS.UnityEngine.Vector3(x, y, z)
+        if effectAsset.IsDone and effectAsset.Asset then
+            local effect = CS.UnityEngine.Object.Instantiate(effectAsset.Asset)
+            effect.transform.position = CS.UnityEngine.Vector3(x, y, z)
 
-        local particle = CS.Minecraft.Lua.LuaUtility.GetParticleSystem(effect)
-        particle:Play()
+            local particle = CS.Minecraft.Lua.LuaUtility.GetParticleSystem(effect)
+            particle:Play()
+        end
 
         self.pending_explosions[key] = nil
     end))
